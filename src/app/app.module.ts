e531f1b91ea29df107619ed03ef149369503a3d0
Daniela AP <daniela.alepos@gmail.com>
2024-01-04 16:19:52 -0600

    Se agrega el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { HomeComponent } from './views/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardsContainerComponent } from './components/cards-container/cards-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    HomeComponent,
+    HeaderComponent,
+    CardsContainerComponent
   ],
   imports: [
     BrowserModule,
